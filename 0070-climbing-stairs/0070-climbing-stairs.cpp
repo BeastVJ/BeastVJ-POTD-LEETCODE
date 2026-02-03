6b9dbcd26cd8c101cf21912d2914507ae8c2d2b0
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-02-04 01:32:35 +0530

    Time: 0 ms (100%), Space: 7.9 MB (55.34%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+int fibHelp(int n, int b, int a){
+    if(n==0 ){
+        return b;
+    }
+    if(n==1){
+        return a;
+    }
+    return fibHelp(n-1, a, a+b);
+}
+int fib(int n){
+    return fibHelp(n, 1,1);
+}
+    int climbStairs(int n) {
+        return fib(n);
+    }
+};
