795d901224c6e00c5c3c6dba8bc7e1b408dc0b81
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-03-12 21:51:33 +0530

    Time: 16 ms (1.03%), Space: 23.6 MB (7.42%) - LeetHub

--- 3861-minimum-capacity-box/3861-minimum-capacity-box.cpp
+++ 3861-minimum-capacity-box/3861-minimum-capacity-box.cpp
@@ -2,19 +2,25 @@ class Solution {
 public:
     int minimumIndex(vector<int>& capacity, int itemSize) {
         // int count = 0;
+        auto init = []() {
+            ios::sync_with_stdio(false);
+            cin.tie(nullptr);
+            cout.tie(nullptr);
+            return 'c';
+        }();
         int n = capacity.size();
         int min = INT_MAX;
         int finalans = -1;
         for (int i = 0; i < n; i++) {
             int currmin = 0;
-            if(capacity[i] >= itemSize){
+            if (capacity[i] >= itemSize) {
                 currmin = capacity[i];
-                if(currmin < min){
+                if (currmin < min) {
                     min = currmin;
                     finalans = i;
                 }
             }
-            cout<<finalans<<endl;
+            cout << finalans << endl;
         }
         return finalans;
     }
