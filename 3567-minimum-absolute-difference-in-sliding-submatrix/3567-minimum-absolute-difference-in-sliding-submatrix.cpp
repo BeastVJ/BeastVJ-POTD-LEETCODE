401312848373c95a9c03f9c2edc235e57e8f7f3f
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-03-20 17:14:18 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
+++ 3567-minimum-absolute-difference-in-sliding-submatrix/3567-minimum-absolute-difference-in-sliding-submatrix.cpp
@@ -1,23 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
-        
+        auto init = []() {
+            ios::sync_with_stdio(false);
+            cin.tie(nullptr);
+            cout.tie(nullptr);
+            return 'c';
+        }();
+
         int m = grid.size();
         int n = grid[0].size();
 
-        vector<vector<int>> result(m - k + 1, vector<int>(n - k +1 ));
+        vector<vector<int>> result(m - k + 1, vector<int>(n - k + 1));
 
-        for(int i = 0; i<= m-k; i++){
-            for(int j = 0; j <= n-k ; j++){
+        for (int i = 0; i <= m - k; i++) {
+            for (int j = 0; j <= n - k; j++) {
                 set<int> val;
 
-                for(int r = i; r <= i + k - 1; r++){
-                    for(int c = j; c <= j + k -1 ; c++){
+                for (int r = i; r <= i + k - 1; r++) {
+                    for (int c = j; c <= j + k - 1; c++) {
                         val.insert(grid[r][c]);
                     }
                 }
 
-                if(val.size() == 1){
+                if (val.size() == 1) {
                     result[i][j] = 0;
                     continue;
                 }
@@ -26,7 +32,7 @@ public:
                 auto prev = val.begin();
                 auto curr = next(prev);
 
-                while(curr != val.end()){
+                while (curr != val.end()) {
                     minDiff = min(minDiff, *curr - *prev);
                     prev = curr;
                     curr++;
