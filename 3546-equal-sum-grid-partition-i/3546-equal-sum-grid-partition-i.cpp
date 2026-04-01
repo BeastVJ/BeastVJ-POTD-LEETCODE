96542bb14948c757277d447e6ad10b7960a99f78
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-04-01 23:27:06 +0530

    Time: 15 ms (27.32%), Space: 130.2 MB (27.32%) - LeetHub

--- 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
+++ 3546-equal-sum-grid-partition-i/3546-equal-sum-grid-partition-i.cpp
@@ -1,31 +1,45 @@
 class Solution {
 public:
+    typedef long long ll;
     bool canPartitionGrid(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
-        long long total = 0;
+        // vijay singh bisht
+        // 11:10
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<ll> rowSum(m, 0);
+        vector<ll> colSum(n, 0);
+        ll total = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                total += grid[i][j];
+
+                rowSum[i] += grid[i][j];
+                colSum[j] += grid[i][j];
+            }
+        }
+
         
-        for (auto &row : grid)
-            for (int x : row)
-                total += x;
-        
-        if (total % 2) return false;
-        
-        long long target = total / 2, sum = 0;
-        
+        if (total % 2 != 0) {
+            return false;
+        }
+        // horizontal
+        ll upper = 0;
         for (int i = 0; i < m - 1; i++) {
-            for (int j = 0; j < n; j++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+            upper += rowSum[i];
+            if (upper == total - upper) {
+                return true;
+            }
         }
-        
-        sum = 0;
-        
+
+        // vertical
+        ll left = 0;
         for (int j = 0; j < n - 1; j++) {
-            for (int i = 0; i < m; i++)
-                sum += grid[i][j];
-            if (sum == target) return true;
+            left += colSum[j];
+            if (left == total - left) {
+                return true;
+            }
         }
-        
         return false;
     }
 };
