f625e7a4218914f1b92b110ea9f74f61438c4d4d
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-04-07 17:44:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -1,20 +1,39 @@
-using ll = long long;
 class Solution {
 public:
-    ll minNumberOfSeconds(int height, vector<int>& times) {
-        ll lo = 1, hi = 1e16;
+    typedef long long  ll;
+    // vijay singh bisht
+    // 5:04 at library
+// this question is based on minimizing the maximum or maximizing the minimum so in this type of question we apply binary search
+    
+     bool Check(ll mid, vector<int>& workerTimes, int mH) {
+        ll h = 0;
 
-        while (lo < hi) {
-            ll mid = (lo + hi) >> 1;
-            ll tot = 0;
-            for (int i = 0; i < times.size() && tot < height; i++)
-                tot += (ll)(sqrt((double)mid / times[i] * 2 + 0.25) - 0.5);
-            if (tot >= height)
-                hi = mid;
-            else
-                lo = mid + 1;
+        for(int &t : workerTimes) {
+            h += (ll)(sqrt(2.0 * mid/t + 0.25) - 0.5);
+
+            if(h >= mH) {
+                return true;
+            }
         }
 
-        return lo;
+        return h >= mH;
+    }
+    long long minNumberOfSeconds(int mountainHeight, vector<int>& workerTimes) {
+        int maxTime = *max_element(begin(workerTimes), end(workerTimes));
+        ll l = 1;
+        ll r = (ll)maxTime * mountainHeight * (mountainHeight+1)/2;
+        int result  = 0;
+
+        // khandani template of binary search
+        while(l <= r){
+            ll mid = l + (r-l)/2;
+            if(Check(mid, workerTimes, mountainHeight)){
+                result = mid;
+                r = mid - 1;
+            }else{
+                l = mid+1;
+            }
+        }
+        return result;
     }
 };
