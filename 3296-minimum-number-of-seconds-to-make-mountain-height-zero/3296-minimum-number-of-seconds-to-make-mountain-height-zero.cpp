42bb310f7d88f3ee2adcb78fdd314f1ebc4a80ac
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-04-07 17:46:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
+++ 3296-minimum-number-of-seconds-to-make-mountain-height-zero/3296-minimum-number-of-seconds-to-make-mountain-height-zero.cpp
@@ -22,20 +22,19 @@ public:
         int maxTime = *max_element(begin(workerTimes), end(workerTimes));
         ll l = 1;
         ll r = (ll)maxTime * mountainHeight * (mountainHeight+1)/2;
+        int result  = 0;
 
-        ll result = 0;
-
-        while(l <= r) {
+        // khandani template of binary search
+        while(l <= r){
             ll mid = l + (r-l)/2;
-
-            if(Check(mid, workerTimes, mountainHeight)) {
+            if(Check(mid, workerTimes, mountainHeight)){
                 result = mid;
-                r = mid-1;
-            } else {
+                r = mid - 1;
+            }else{
                 l = mid+1;
             }
         }
-
         return result;
+
     }
 };
