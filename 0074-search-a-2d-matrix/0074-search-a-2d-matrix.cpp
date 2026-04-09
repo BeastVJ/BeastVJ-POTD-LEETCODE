069614ba5817999e25aa9d7140265a6a7c2c8744
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-04-09 19:27:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -4,17 +4,17 @@ public:
     //7:14 at library
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int m = matrix.size(); //rows ke liye
-        int n = matrix[0].size(); // column ke liye
-
-        for(int i = 0; i< m; i++){
-            for(int j = 0; j< n; j++){
-                if(matrix[i][j] == target){
-                    return true;
-                }
+        int n = matrix[0].size(); 
+        int i = 0;
+        for(int j = n-1; j > 0; j--){
+            if(matrix[i][j]> target){
+                j--;
+            }else if(matrix[i][j] < target){
+                i++;
+            }else{
+                return true;
             }
         }
-
         return false;
 
     }
