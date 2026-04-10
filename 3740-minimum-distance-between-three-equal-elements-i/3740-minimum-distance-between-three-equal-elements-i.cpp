5a34bcec688fee851e4058b476d435907b56212b
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-04-11 00:42:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
     // Vijay Singh Bisht
-    //11:50 at 204
+    // 11:50 at 204
     int minimumDistance(vector<int>& nums) {
-
         int min = INT_MAX;
-        int n = nums.size();
-        for (int i = 0; i < n - 2; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    if (nums[i] == nums[j] && nums[j] == nums[k]) {
-                        int currmin = abs(i - j) + abs(j - k) + abs(k - i);
-                        if (min > currmin) {
-                            min = currmin;
-                        }
-                    }
+        map<int, vector<int>> mp;
+        for (int i = 0; i < nums.size(); i++) {
+            mp[nums[i]].push_back(i);
+        }
+        for (auto it : mp) {
+            if (it.second.size() > 2) {
+                int currMin = abs(it.second[0] - it.second[1]) +
+                              abs(it.second[1] - it.second[2]) +
+                              abs(it.second[2] - it.second[0]);
+                if (min > currMin) {
+                    min = currMin;
                 }
             }
         }
-        if(min > 200) return -1;
+        if(min >200) return -1;
         return min;
     }
 };
