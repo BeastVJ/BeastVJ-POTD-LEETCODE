dd2f1b563f0f6b28bb2ad90c8c429d4535c9de95
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-05-04 15:58:30 +0530

    Time: 4 ms (0.91%), Space: 10.3 MB (8.3%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -6,9 +6,16 @@ public:
         int m = matrix[0].size();
 
         for (int i = 0; i < n; i++) {
-            for (int j = i+1; j < n; j++) {
+            for (int j = i; j < n; j++) {
                 swap(matrix[i][j] , matrix[j][i]);
             }
+        }
+
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << matrix[i][j] << " ";
+            }
+            cout << endl;
         }
         for(int i = 0; i< n; i++){
             reverse(matrix[i].begin(), matrix[i].end());
