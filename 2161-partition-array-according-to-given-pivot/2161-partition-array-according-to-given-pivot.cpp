8590b2961ee7b3ce1cbac2e9dbc75a70cc32f420
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-06-08 21:04:29 +0530

    Time: 6 ms (66.24%), Space: 139.4 MB (19.95%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -4,25 +4,18 @@ public:
         vector<int> first;
         vector<int> second;
         vector<int> last;
+        int count  = 0; 
         for(int it: nums){
             if(it < pivot){
                 first.push_back(it);
             }
-        }
-
-        for(int it: nums){
             if(it > pivot){
                 second.push_back(it);
             }
-        }
-        int count = 0;
-        for(int i = 0 ; i< nums.size(); i++){
-            if(nums[i] == pivot){
+            if(it == pivot){
                 count++;
             }
         }
-        // sort(first.begin(), first.end(), greater<>());
-        // sort(second.begin(), second.end());
         for(int st: first){
             last.push_back(st);
         }
