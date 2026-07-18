ac2da08eb3a30d99f1c192e3023f5a422bff04dc
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-07-18 16:26:02 +0530

    Time: 3 ms (5.8%), Space: 16.3 MB (31.74%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.cpp
@@ -1,10 +1,40 @@
+//Approach 1 
+// class Solution {
+// public:
+//     int findGCD(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         int n = nums.size();
+//         cout<<nums[0]<<" "<<nums[n-1];
+//         int num = gcd(nums[0], nums[n-1]);
+//         return num;
+//     }
+// };
+
+
+
+// Approach 2
 class Solution {
 public:
     int findGCD(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
-        cout<<nums[0]<<" "<<nums[n-1];
-        int num = gcd(nums[0], nums[n-1]);
-        return num;
+        int mn = nums[0];
+        int mx = nums[n-1];
+        int div= 0;
+
+
+        for(int i = 1; i<=mx; i++){
+            // cout<<i<<" ";
+            // int currdiv = i;
+            if(mn % i == 0 && mx % i == 0){
+                div = i;
+                cout<<div<<" ";
+            }
+            // if(currdiv > div ){
+            //     div = currdiv;
+            //     // cout<<div<<" ";
+            // }
+        }
+        return div;
     }
 };
