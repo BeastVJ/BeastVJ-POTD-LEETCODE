b5ab4cc5734fb07db96666e83718919693646911
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-07-21 16:26:31 +0530

    Time: 5 ms (24.24%), Space: 18.2 MB (39.2%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ 1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -1,34 +1,68 @@
+// class Solution {
+// public:
+//     //vijay singh bisht
+//     //at midway bhimtal
+//     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
+//         vector<int> oned;
+
+//         int m = grid.size();
+//         int n = grid[0].size();
+//         for (const auto& row : grid) {
+//             for (auto element : row) {
+//                 oned.push_back(element);
+//             }
+//         }
+//         k %= oned.size();
+
+//         reverse(oned.begin(), oned.end());
+
+//         reverse(oned.begin(), oned.begin() + k);
+
+//         reverse(oned.begin() + k, oned.end());
+
+//         vector<vector<int>> ans(m, vector<int>(n));
+
+//         int idx = 0;
+//         for (int i = 0; i < m; i++) {
+//             for (int j = 0; j < n; j++) {
+//                 ans[i][j] = oned[idx++];
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+// Approach 2
+
+
 class Solution {
 public:
-    //vijay singh bisht
-    //at midway bhimtal
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
-        vector<int> oned;
 
-        int m = grid.size();
-        int n = grid[0].size();
-        for (const auto& row : grid) {
-            for (auto element : row) {
-                oned.push_back(element);
-            }
-        }
-        k %= oned.size();
 
-        reverse(oned.begin(), oned.end());
+        int row = grid.size();
+        int col = grid[0].size();
 
-        reverse(oned.begin(), oned.begin() + k);
+        int n = row*col;
 
-        reverse(oned.begin() + k, oned.end());
-
-        vector<vector<int>> ans(m, vector<int>(n));
-
-        int idx = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                ans[i][j] = oned[idx++];
-            }
+        k = k % n;
+        if(k== 0){
+            return grid;
         }
 
-        return ans;
+        auto reverse= [&](int i, int j){
+            while(i < j){
+                swap(grid[i/ col][i % col], grid[j/col][j % col]);
+                i++;
+                j--;
+            }
+        };
+
+        reverse(0, n-1);
+        reverse(0, k-1);
+        reverse(k, n-1);
+
+        return grid;
     }
 };
