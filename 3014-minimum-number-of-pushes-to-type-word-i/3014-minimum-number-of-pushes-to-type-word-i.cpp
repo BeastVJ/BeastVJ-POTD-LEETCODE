eb641f8f4030a12bcedb3158c74b63b5f73e8a3a
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-08-06 12:44:41 +0530

    Time: 4 ms (2.53%), Space: 8.9 MB (10.24%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,11 +1,22 @@
 class Solution {
 public:
+    // vijay singh bisht
+    // at midway
     int minimumPushes(string word) {
-        int len = word.length();
-        int res = 0;
-        for(int i = 0; i< len; i++){
-            res += floor(i / 8) + 1;
+        int result = 0;
+        unordered_map<int, int> mp;
+        int key = 2;
+        for(char &ch: word){
+            if(key> 9){
+                key = 2;
+            }
+
+            mp[key]++;
+            result += mp[key];
+
+            key++;
         }
-        return res;
+
+        return result;
     }
 };
