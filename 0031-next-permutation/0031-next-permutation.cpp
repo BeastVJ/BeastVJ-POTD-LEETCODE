3a95a5e8497f98565c4bf5a04e8ea6d97b78f6f1
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-08-08 13:15:44 +0530

    Time: 0 ms (100%), Space: 15.6 MB (79.59%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // vijay singh bisht
+    // at midway
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int idx = -1;
+
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                idx = i - 1;
+                break;
+            }
+        }
+
+        if (idx != -1) {
+            int swap_idx = -1;
+            for (int j = n - 1; j >= idx+1; j--) {
+                if (nums[j] > nums[idx]) {
+                    swap_idx = j;
+                    break;
+                }
+            }
+            swap(nums[idx], nums[swap_idx]);
+        }
+
+        reverse(nums.begin() + idx + 1, nums.end());
+    }
+};
