b706fe242b98876334e89ae4c85bfbb4ec7dd482
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-08-09 11:30:08 +0530

    Time: 0 ms (100%), Space: 10.4 MB (85.81%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,6 +1,21 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
+        sort(piles.begin(), piles.end());
+        int aSum = 0;
+        int bSum = 0;
+        for (int i = 0; i < piles.size(); i++) {
+            if (i % 2 == 0) {
+                aSum += piles[i];
+            } else {
+                bSum += piles[i];
+            }
+        }
+        cout<<aSum<<"  "<<bSum;
+        if (aSum > bSum) {
+            return false;
+        }
+
         return true;
     }
 };
