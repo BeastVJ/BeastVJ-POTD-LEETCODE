a46b6a5aae0c244222137961ec9f5d9707181c6f
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-08-09 12:18:51 +0530

    Time: 10 ms (23.87%), Space: 11.6 MB (30.84%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-    bool stoneGame(vector<int>& piles) {
-        sort(piles.begin(), piles.end());
-        int aSum = 0;
-        int bSum = 0;
-        for (int i = 0; i < piles.size(); i++) {
-            if (i % 2 == 0) {
-                aSum += piles[i];
-            } else {
-                bSum += piles[i];
-            }
-        }
-        cout<<aSum<<"  "<<bSum;
-        if (aSum > bSum) {
-            return false;
+    int t[501][501];
+
+    int solve(vector<int>& piles, int i, int j) {
+        if (i > j){
+            return 0;
         }
 
-        return true;
+        if (t[i][j] != -1) {
+            return t[i][j];
+        }
+
+        int take_i =
+            piles[i] + min(solve(piles, i + 2, j), solve(piles, i + 1, j - 1));
+
+        int take_j =
+            piles[j] + min(solve(piles, i, j - 2), solve(piles, i + 1, j - 1));
+
+        return t[i][j] = max(take_i, take_j);
+    }
+    bool stoneGame(vector<int>& piles) {
+        
+        int n = piles.size();
+
+        memset(t, -1, sizeof(t));
+        int sum = accumulate(begin(piles), end(piles), 0);
+
+        int Alice_Score = solve(piles, 0, n-1);
+        return Alice_Score > sum/2;
+
     }
 };
